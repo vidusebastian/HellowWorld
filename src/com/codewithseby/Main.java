04928485d417d770c75366c503f3a73ddb822b49
Sebastian VIDU <viduseba@gmail.com>
2020-12-28 15:34:46 +0200

    Divizori numar

--- src/com/codewithseby/Main.java
+++ src/com/codewithseby/Main.java
@@ -8,14 +8,23 @@ package com.codewithseby;
 public class Main {
 
     public static void main(String[] args) {
-        int ctr = 0;
-        for (int i = 1; i <= 25; i++) {
-
-            if (5 % i == 0 && i*i!=25) {
-                ctr += 2;
-
-            }
-        }
-        System.out.println (ctr);
+        int x = 25;
+        System.out.println(mymethod(x));
     }
+
+    public static int mymethod(int number) {
+int ctr=0;
+        for (int i = 1; i <= Math.sqrt(number); i++) {
+            if ( number % i ==0 && i*i!=number) {
+                ctr+=2;
+            } else if (i*i==number) {
+                ctr++;
+            }
+
+
+        }
+
+        return ctr;
+    }
+
 }
--- src/com/codewithseby/Numaruldedivizoriaiunuinumar.java
+++ src/com/codewithseby/Numaruldedivizoriaiunuinumar.java
@@ -12,15 +12,15 @@ public class Numaruldedivizoriaiunuinumar {
         for(int i=1; i<=(int)Math.sqrt(num); i++) {          // Generam intervalul de numere care pot fi divizorii unui numar dat
             //{  1 , 2 ......... 25}
             if(num % i==0 && i*i!=num) {                     // Daca 25 % 1   == 0 . Rest 0 0 =0 //   TRUE i = 1 , 5 , 25
-                                                             //  1 * 1 = 1  diferit de 25
-                                                             //  5 * 5 = 25  = 25
-                                                             //  25 * 25 = 625 diferit de 25
-                                                             // Deci de 2 ori este adevarat , de face loop-ul de 2 ori TRUE.
-                ctr+=2;                                      // Aduna 2  + 2  = 4 la ctr.   DAr ne da 3 ????? De ce ?
+                                                             //  1 * 1 = 1  diferit de 25  TRUE
+                                                             //  5 * 5 = 25  = 25   FALSE
+                                                             //  25 * 25 = 625 diferit de 25 TRUE
+                                                             // Deci o data este TRUE , deci face loop-ul odata.
+                ctr+=2;                                      // Adunam 2  la variabila ctr  = 2 (face overide)
 
 
-            } else if (i*i==num) {
-                ctr++;
+            } else if (i*i==num) {                           // 1* 1 = 25 FALSE  5*5= 25  TRUE    25 * 25 = 625 = 25 FALSE
+                ctr++;                                       // Adunam 1 la ctr
             }
         }
         return ctr;
