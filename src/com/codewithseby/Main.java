ee825250fb0834257852f5bb785be866c8db5b85
Sebastian VIDU <viduseba@gmail.com>
2020-12-29 19:02:24 +0200

    Comenzi munca uzuale

--- src/com/codewithseby/Main.java
+++ src/com/codewithseby/Main.java
@@ -1,19 +1,20 @@
 package com.codewithseby;
 
 /*
-64. Write a Java program that accepts two integer values between 25 to 75 and return true if there is a common digit in both numbers. Go to the editor
-Sample Output:
-
-Input the first number : 35
-Input the second number: 45
-Result: true
-
+86. Write a Java program start with an integer n, divide n by 2 if n is even or multiply by 3 and add 1 if n is odd,
+repeat the process until n = 1. Go to the editor
  */
 
-import java.util.Scanner;
+import java.util.Arrays;
 
 public class Main {
     public static void main(String[] args) {
+int n;
+
+ if  (n % 2 == 0 ) {
+
+ }
+
 
 
     }
