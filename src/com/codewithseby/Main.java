b251648d8772f4deac4107591bca18c42a58724b
Sebastian VIDU <viduseba@gmail.com>
2020-12-29 11:08:52 +0200

    loop

--- src/com/codewithseby/Main.java
+++ src/com/codewithseby/Main.java
@@ -1,22 +1,33 @@
 package com.codewithseby;
+
 /*
-58. Write a Java program to capitalize the first letter of each word in a sentence. Go to the editor
+64. Write a Java program that accepts two integer values between 25 to 75 and return true if there is a common digit in both numbers. Go to the editor
 Sample Output:
 
-Input a Sentence: the quick brown fox jumps over the lazy dog.
-The Quick Brown Fox Jumps Over The Lazy Dog.
-
+Input the first number : 35
+Input the second number: 45
+Result: true
 
  */
-
-import java.util.Locale;
 import java.util.Scanner;
 
 public class Main {
-
     public static void main(String[] args) {
-Convertesteinliteramarefiecarecuvant obj = new Convertesteinliteramarefiecarecuvant();
-obj.Convertesteinliteramarefiecarecuvant("             ");
 
+        int weeks = 3;
+        int days = 7;
+
+        // outer loop
+        for(int i = 1; i <= weeks; ++i) {
+            System.out.println("Week: " + i);
+
+            // inner loop
+            for(int j = 1; j <= days; ++j) {
+
+                // continue inside the inner loop
+
+                System.out.println(i+ "  Days: " + j);
+            }
+        }
     }
 }
--- src/com/codewithseby/Numeretreicifreunice.java
+++ src/com/codewithseby/Numeretreicifreunice.java
@@ -24,7 +24,10 @@ public class Numeretreicifreunice {
 
             for (int j = 1; j <= 4; j++) {    //i = 1,2,3,4 // Loop-ul se va repeta de 4 ori
 
-                for (int k = 1; k <= 4; k++) {      //i = 1,2,3,4 // Loop-ul se va repeta de 4 ori
+                for (int k = 1; k <= 4; k++) {      //i = 1,2,3,4 // Loop-ul se va repeta de 4 ori. Ia valoarea 1 de la primele 2 loop-uri
+                    // dupa care repeta ultimul loop de 4 ori pana da false. Dupa aceea ia valoarea 1 de la primul loop si  2 de la al
+                    // doilea loop si iar repeta ultimul loop complet cu 1 , 2 ,3 ,4.
+                    //Adica ordinea de executare a loop-urilor este de la inner loop la principal loop.
                     if (k != i && k != j && i != j) {         //Ii zici ca vrei sa fie numere unice .
                         amount++;
                         System.out.println(i + "" + j + "" + k);   // Printeze numerele de cate 3 cifre. Trebuie pus "" spatiu intre cifre ca sa nu le adune.
