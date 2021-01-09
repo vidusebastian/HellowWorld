5428c4c58ea184a80cede84c8c558850aa6e08c7
Sebastian VIDU <viduseba@gmail.com>
2021-01-09 12:15:29 +0200

    Oracle free tutorial

--- src/com/codewithseby/Main.java
+++ src/com/codewithseby/Main.java
@@ -3,17 +3,21 @@ package com.codewithseby;
 
 public class Main {
     public static void main(String args[]) {
-        double tax = 0.2, total = 0.0;
-        System.out.println("Welcome to Duke CHoice shop");
+        double tax = 0.2;
+        double total = 0.0;
+
+        //  System.out.println("Welcome to Duke CHoice shop");
         Customer c1 = new Customer();
         c1.name = "Pinky";
         c1.size = "S";
-        System.out.println("Customer is " + c1.name);
+        // System.out.println("Customer is " + c1.name);
 
         Clothing item1 = new Clothing();
         Clothing item2 = new Clothing();
 
-        Clothing [] items = { item1 , item2 };  // Array de obiecte
+
+        Clothing[] items = {item1, item2, new Clothing(), new Clothing()};  // Array de obiecte  .  se pot declara obiectele
+        // direct in array cu numele new Clothing () si nu mai trebuie sa le declaram in liniile de mai sus
         // items[0].size = "J";  // Schimbam size-ul pentru primul obiect din Array adica item1.size = "J" .
 
         item1.description = "Blue Jacket";
@@ -25,29 +29,56 @@ public class Main {
         item2.price = 10.5;
         item2.size = "S";
 
+        items[2].description = "Green Scarf";
+        items[2].price = 5;
+        items[2].size = "S";
 
-        System.out.println("Item1" + "," + item1.description + "," + item1.price + "," + item1.size);
-        System.out.println("Item2" + "," + item2.description + "," + item2.price + "," + item2.size);
+        items[3].description = "Blue T-Shirt";
+        items[3].price = 10.5;
+        items[3].size = "S";
 
-        total = (item1.price + item2.price * 2) * (1 + tax);
 
+        // System.out.println("Item1" + "," + item1.description + "," + item1.price + "," + item1.size);
+        // System.out.println("Item2" + "," + item2.description + "," + item2.price + "," + item2.size);
+
+        //Calculeaza totalul
+        //   total = (item1.price + item2.price * 2) * (1 + tax);
+
+        //  System.out.println("Total is : " + total);
+
+        int measurement = 3;
+
+        switch (measurement) {
+            case 1:
+            case 2:
+            case 3:
+                c1.size = "S";
+                break;
+            case 4:
+            case 5:
+            case 6:
+                c1.size = "R";
+                break;
+            case 7:
+            case 8:
+            case 9:
+                c1.size = "L";
+                break;
+            default:
+                c1.size = "X";
+        }
+        for (Clothing item : items) {
+            if (c1.size.equals(item.size )) {
+                total = total + item.price;  // 0 + 20.9 = 20.9   // total = 20.9 + 10.5 = 31.4 // total = 31.4
+                System.out.println("Item" + "," + item.description + "," + item.price + "," + item.size);
+            if (total> 15) {
+                break;
+              }
+            }
+
+
+        }
         System.out.println("Total is : " + total);
 
-        int measurement = 3 ;
-
-         switch (measurement) {
-             case 1: case 2: case 3:
-                 c1.size= "S";
-                 break;
-             case 4: case 5: case 6:
-                 c1.size = "R";
-                 break;
-             case 7: case 8: case 9:
-                 c1.size="L";
-                 break;
-             default:
-                 c1.size="X";
-         }
     }
-
 }
