dc60d3e06f6186b9765be58a2f2d5d4f5b76b134
Sebastian VIDU <viduseba@gmail.com>
2021-01-17 16:20:25 +0200

    Palindrom

--- src/com/codewithseby/Palindromverif.java
+++ src/com/codewithseby/Palindromverif.java
@@ -2,6 +2,12 @@ package com.codewithseby;
 
 import java.util.Arrays;
 
+// 1. Convertim stringul introdus intr-un array de caractere
+// 2. Copiem valorile array-ului original in alt array.
+// 3. Loop prin primul array  dar pana la jumatatea indexului adica la litera din mijloc si  Arrayul original devine reverse
+// 4. Comparam cele 2 arrayuri cu equals si printam daca este sau nu palindrom
+
+
 public class Palindromverif {
     public static void main(String args[]) {
 
@@ -10,9 +16,9 @@ public class Palindromverif {
         char[] myArray = sintrodus.toCharArray();
         char [] original = Arrays.copyOf(myArray,myArray.length);
 
-        for (int i = 0; i < myArray.length / 2; i++) {
-            char temp = myArray[i];
-            myArray[i] = myArray[myArray.length-i-1];
+        for (int i = 0; i < myArray.length / 2; i++) {  // Imparte la 2 ca sa ajunga la indexul pana la litera din mijloc fara ea.
+            char temp = myArray[i];        // Salveaza valorile array-ului in variabila char
+            myArray[i] = myArray[myArray.length-i-1];       // Override la indexul array-ului Original
             myArray[myArray.length-i-1] = temp;
         }
         System.out.println("Original Array"+Arrays.toString(original));
