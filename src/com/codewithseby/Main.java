0532feea63a2544f9f82bd7b324ad5d1df295f2f
Sebastian VIDU <viduseba@gmail.com>
2021-01-23 13:40:43 +0200

    Munca Comenzi

--- src/com/codewithseby/Main.java
+++ src/com/codewithseby/Main.java
@@ -3,9 +3,22 @@ package com.codewithseby;
 
 
 public class Main {
+    /**
+     *
+     * @param args comand line arguments
+     */
 
+    public static void main(String[] args) {
+        System.out.println("args-size= " + args.length);
 
-    public static void main(String args[]) {
+        for ( int i = 0 ; i < args.length; i++) {
+            System.out.println(" args [" + i + " ] =" + args[i]);
+        }
+        sum()
+    }
 
+  
+    public static int  sum (int a, int b) {
+        return a + b;
     }
 }
