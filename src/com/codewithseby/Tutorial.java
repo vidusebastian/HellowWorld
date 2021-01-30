cf3d5fbba0e9d51bc60bf7d2605ca0a2c0eca6a4
Sebastian VIDU <viduseba@gmail.com>
2021-01-30 16:14:39 +0200

    Loop fara acolade explicat

--- src/com/codewithseby/Tutorial.java
+++ src/com/codewithseby/Tutorial.java
@@ -338,3 +338,4 @@ public class Main {
  */
 
 //switch-ul printeaza de la match case pana la primul break;
+// Loop fara acolade inseamna ca print statmentul din loop va fi in afara loop-ului. ADica se va calca variabila pana la final si dupa aceea o printam doar o singura data, nu de cate ori se face loop-upul.
