0d7067329c0251247d7f72d03bbf3492db674161
Sebastian VIDU <viduseba@gmail.com>
2021-01-31 16:58:34 +0200

    Palindrom verificare

--- src/com/codewithseby/Palindromverif.java
+++ src/com/codewithseby/Palindromverif.java
@@ -31,3 +31,55 @@ public class Palindromverif {
         }
     }
 }
+
+
+/*
+Sau varianta mea 2
+package com.codewithseby;
+
+import java.util.Arrays;
+import java.util.Locale;
+import java.util.Random;
+
+public class Main {
+
+    public static void main(String[] args) {
+        // A B A C
+        // 0 1 2 3
+        String original = "ABAC";
+        char[] ch = original.toCharArray();
+        System.out.println("ch.lenght = " + ch.length);
+
+        char[] reversea = new char[ch.length];
+        int i = 0;
+        for (i = ch.length - 1; i >= 0; i--) {   // i = 3 , 2 , 1 , 0   // ch.lenght = 4
+            //   System.out.println(ch[i]);
+            reversea[ch.length - i - 1] = ch[i];              //  ceva = 0 , 1 , 2 , 3
+        }
+
+
+        String x = Arrays.toString(ch);
+        System.out.println("x " + x);
+
+        String y = Arrays.toString(reversea);
+
+        System.out.println("y " + y);
+
+//        if (x.equals(y)) {
+//            System.out.println("Palindrom");
+//        } else {
+//            System.out.println("Is not palindrom");
+//        }
+
+        if (Arrays.equals(ch, reversea)) {
+            System.out.println("palindrom");
+        } else {
+            System.out.println("Not palindrom");
+
+        }
+    }
+}
+
+
+
+ */
