6133425fdcf9d3d1ea77f060d8b488be9556b150
Sebastian VIDU <viduseba@gmail.com>
2021-02-14 15:17:38 +0200

    zzz

--- src/com/codewithseby/CristiExamen.java
+++ src/com/codewithseby/CristiExamen.java
@@ -10,7 +10,7 @@ public class CristiExamen {
 
 
 
-
+// Disable autocomplete ??
 
 
 
--- src/com/codewithseby/Main.java
+++ src/com/codewithseby/Main.java
@@ -1,18 +1,15 @@
 package com.codewithseby;
 
 
+import java.util.*;
+import java.io.*;
+
 public class Main {
+
+
+
     public static void main(String[] args) {
 
-        int total = 0;
-        StringBuilder letters = new StringBuilder("abcdefg");
-        total += letters.substring(1, 2).length();
-        System.out.println(total);
-        total += letters.substring(6, 5).length();
-        System.out.println(total);
 
     }
 }
-
-
-
